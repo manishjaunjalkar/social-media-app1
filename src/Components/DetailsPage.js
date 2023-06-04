71a38581e8c2b1d1bc10d69b38201c65d3249a96
manishjaunjalkar <manishvjaunjalkar@gmail.com>
2023-06-04 22:45:06 +0530

    initial commit

---
+++ src/Components/DetailsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+
+const DetailsPage = () => {
+  const { post } = useSelector((state) => state.detailsCard);
+  if(!post) return <h1> Wait for a sec Loading...</h1>
+  
+  return (
+    <div className='details-container'>
+      <h2>Page For Post With ID {post.id}</h2>
+      <img src={`https://picsum.photos/200?random=${post.id}`} alt={post.title} />
+      <p>User Id: {post.userId}</p>
+      <h3>Title: {post.title}</h3>
+      <p>Body: {post.body}</p>
+  </div>
+  )
+}
+
+export default DetailsPage
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import postsReducer from "./postsReducers";
+import detailsReducer from "./detailsReducer";
+
+const rootReducer = combineReducers({
+    posts: postsReducer,
+    detailsCard: detailsReducer
+})
+
+export default rootReducer;
